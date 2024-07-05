5d509a0b12a28eb3a69d9503fcf817ad9f95f26d
Panudach Kongsomboon <120073076+AumPanudach@users.noreply.github.com>
2024-07-06 02:10:04 +0700

    day 2

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Box } from "@mui/material";
+export const dynamic = "force-dynamic";
+type Props = {
+  children: React.ReactNode;
+};
 
-type Props = {}
-
-export default function layout({}: Props) {
-  return (
-    <div>layout</div>
-  )
+export default function Defaultlayout({children}: Props) {
+  return <section>
+    <Box sx={{display:"flex"}}>
+        <Box component="main" sx={{flexGrow:1,p:3}}>
+            {children}
+        </Box>
+    </Box>
+  </section>;
 }
