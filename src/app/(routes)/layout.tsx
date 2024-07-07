be3be2cbd4dc4645641ebbcb4c6a1129fa1fcb1d
Panudach Kongsomboon <120073076+AumPanudach@users.noreply.github.com>
2024-07-08 00:37:48 +0700

    day 3

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,16 +1,31 @@
+"use client"
 import React from "react";
 import { Box } from "@mui/material";
 export const dynamic = "force-dynamic";
+import Header from "../_components/layout/Header";
+import Sidebar from "../_components/layout/Sidebar";
+import DrawerHeader from "../_components/layout/DraweHeader";
 type Props = {
   children: React.ReactNode;
 };
 
 export default function Defaultlayout({children}: Props) {
-  return <section>
+const [open,setOpen] = React.useState(true);
+const handleDrawerOpen = () => {
+  setOpen(true)
+};
+const handleDrawerClose = () => {
+  setOpen(false)
+};
+////////////////////////////////////////////////////////
+ return (<section>
     <Box sx={{display:"flex"}}>
+      <Header open={open} handleDrawerOpen={handleDrawerOpen}/>
+      <Sidebar open={open} handleDrawerClose={handleDrawerClose}/>
         <Box component="main" sx={{flexGrow:1,p:3}}>
+          <DrawerHeader/>
             {children}
         </Box>
     </Box>
-  </section>;
+  </section>);
 }
